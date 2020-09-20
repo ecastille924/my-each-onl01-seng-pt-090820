a0e34182eeb39301648d5c9d71360bbbd6941724
Erick Castille <ecastille924@gmail.com>
2020-09-20 19:55:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
   i = 0
   while i < array.length
-    returned_array << yield(array[i])
+    collection << yield(array[i])
     i = i + 1
   end
-  returnd_array
+  collection
 end
